5af56302af1ee1d7f87c8894fbf282be30d57a33
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2025-07-10 22:28:11 +0530

    PrimeNumber.c

--- PrimeNumber.c
+++ PrimeNumber.c
@@ -1,4 +1,26 @@
 #include<stdio.h>
 int main(){
+  int num,isprime=1,i;
+  printf("enter a number");
+  scanf("%d",&num);
+  if(num<=0){
+    isprime=0;
+  }
+  else{
+    for(i=2;i<=num/2;i++){
+      if(num%i==0){
+        isprime=0;
+        break;
+      }
+    }
+  }
+  if(isprime==0){
+    printf("not a prime number");
+  }
+  else{
+    printf("prime number");
+  }
+        
+  
   return 0;
 }
